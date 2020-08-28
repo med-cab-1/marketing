dacc99477201771e77f52ce2557b5385099e9720
JoshuaHolloway <jhollow6@asu.edu>
2020-08-28 05:25:53 -0500

    card dispensary section

--- deploy/public/home/comps/disp-1.js
+++ deploy/public/home/comps/disp-1.js
@@ -1,59 +1,24 @@
 const template = document.createElement('template');
 template.innerHTML = `
     <style>
-
-        /* fill remaing space vertically with flexbox: */
-        /* https://codepen.io/JoshuaHolloway/pen/rNemPom */
-
-        ._popular-strain_{
-            height: 500px;
-            /* width: 500px; */
-            border: solid red 5px;
-            display: flex;
-            flex-direction: column;
-            
-            background-color: var(--color-primary);
-        }
+        @import url('./css/card.css');
 
         ._top_ {
-            border: solid blue 5px;
-            height: 70%;
-
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center;
-            background-image: url('img/dispensary-1.png');
-
+            background-image: url('./img/disp-1.png');
         }
-
-        ._bottom_ {
-            border: solid orange 5px;
-            flex: 1;
-        }
-        ._bottom_ > h5 {
-            font-size: 2.4rem;
-            text-align: center;
-        }
-
-        ._top_, ._bottom_ {
-            display: grid;
-            place-items: center;
-        }
-
     </style>
 
 
-            <div class="_popular-strain_">
+    <div class="_container_">
 
-                <div class="_top_">
-                </div>
+        <div class="_top_">
+        </div>
 
-                <div class="_bottom_">
-                    <h5>Silver Haze</h5>
-                </div>
-                
-            </div>
-
+        <div class="_bottom_">
+            <p>Silver Haze</p>
+        </div>
+        
+    </div>
 `;
 
 class WebComp extends HTMLElement {
