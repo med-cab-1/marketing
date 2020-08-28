4f71d5d318980c3bfe62d54186c2e5dbe986758b
JoshuaHolloway <jhollow6@asu.edu>
2020-08-27 20:55:03 -0500

    added josh's code to main branch

--- landing-page/comps/web-component-nested2/main.js
+++ landing-page/comps/web-component-nested2/main.js
@@ -1,26 +1,24 @@
-const template = document.createElement('template');
-template.innerHTML = `
-    <style>
-        h3: {
-            color: green;
-        }
-    </style>
+import {Button} from './button.js';
 
-    <h3>Josh</h3>
+// =================================================
+
+const template = document.createElement('template');
+const x = 'josh';
+
+template.innerHTML = `
+    Inside main4.js:
+    <button-comp name="${x}"></button-comp>
 `;
 
 class WebComp extends HTMLElement {
     constructor() {
         super();
-
         this.attachShadow({ mode: 'open' });
         const deep_copy = true; // include descendants
         this.shadowRoot.appendChild(template.content.cloneNode(deep_copy));
-
-        // this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
-
-        // this.innerHTML = ``;
     }
 }
+
+
 
 window.customElements.define('web-comp', WebComp);
